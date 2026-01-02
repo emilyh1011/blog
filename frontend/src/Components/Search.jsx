a26a7c9e726e86f8a51face6ea1c44fd81c7d642
EMILY HAN <eh3244@nyu.edu>
2026-01-02 11:29:29 -0500

    Fixed search and filter logic with effect

--- frontend/src/Components/Search.jsx
+++ frontend/src/Components/Search.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { IoIosSearch } from "react-icons/io";
 import {useState} from 'react';
 //Left off 37.18 at axios
-function Search({selectedTag, onSearch}) {
+function Search({onSelectedTag, onSearch}) {
     /*Pass a selectedTag prop to Search component and destructure it so we can access the actual value.
     This will be the tag/section that the user is currently selecting */
 
-    {/*Create the different "tags" to filter btwn diff sections of blog.
-    Treat as array of section objects. Just make sure you have same kinds of attributes
-    for each section*/}
     const tags=[
         {
             id:1,
@@ -36,44 +33,27 @@ function Search({selectedTag, onSearch}) {
 
     //Define a searchQuery state, even though we already have this in Home.jsx, we will just "life the state" up to parent
     //Search bar starts empty
-    const [searchQuery, setSearchQuery] = useState(null);
-
-    {/*Set flex direction to columns. Before search bar and banner in a row,
-    but setting to columns means we are stacking vertically. So search will go
-    go below banner*/}
-    {/*Less padding when screen smaller and more padding when bigger*/
-    /*Before, we were using px for padding for small size and md and above,
-    but screen wasn't adjusting smoothly bc my picture would shrink but then get
-    bigger to account for less margin.
-    Now, just use %, so margin always stays proportional to screen*/
-    }
+    const [searchQuery, setSearchQuery] = useState("");
+   
   return (
     <div className = 'flex flex-col items-center w-3/5'>
-        {/*Center banner, add margin to top of banner*/}
         <img src= "https://res.cloudinary.com/dwhtlckoy/image/upload/v1767125407/SearchBarPhotoEABungee_d5mzav.jpg" className = 'rounded-2xl w-full'/>
 
-        {/*Actual search bar, wrap it in a div*/}
-        {/*Remember, width full to extend search bar to fill whole container, but 5% padding of container
-        applied and we apply our own padding to search bar to make it smaller */}
-        {/*Remember flex puts everything in one line, so we put our search icon and Search placeholder in same line */}
-        {/*Adjusting text size for an icon is same as adjusting size of icon */}
-        <div className = 'flex bg-white shadow-lg items-center w-3/4 p-2 -mt-6 rounded-md sm:p-3 sm:-mt-7 sm:rounded-lg md:p-4 md:-mt-8 md:rounded-lg lg:p-6 lg:-mt-9 lg:rounded-xl'>
-            <IoIosSearch className= 'text-[14px] sm:text-[16px] md:text-[20px] text-gray-400' />
-            <input type = 'text' placeholder= {searchQuery} className = 'text-[13px] sm:text-[14px] md:text-[16px] outline-none ml-2 w-full' 
-            onChange={(event)=>{
-                setSearchQuery(event.target.value); //Every time user changes input in search bar, we want to save this as a possible search query
-                
-            }}
-            //Only on enter keydown, we want to send "search", call our onSearch function prop which will call 
-            //handleSearch in parent Home.jsx to handle our search logic
-            onKeyDown={(event)=>{
-                if(event.key == "Enter"){
+        <form 
+            onSubmit={(event)=>{
+                event.preventDefault();
+                if(searchQuery.trim() != ""){
                     onSearch(searchQuery); //Lift our searchQuery state to Home.jsx
                 }
             }}
-            
+            className = 'flex bg-white shadow-lg items-center w-3/4 p-2 -mt-6 rounded-md sm:p-3 sm:-mt-7 sm:rounded-lg md:p-4 md:-mt-8 md:rounded-lg lg:p-6 lg:-mt-9 lg:rounded-xl'>
+            <IoIosSearch className= 'text-[14px] sm:text-[16px] md:text-[20px] text-gray-400' />
+            <input type = 'text' placeholder="Search..." value= {searchQuery} className = 'text-[13px] sm:text-[14px] md:text-[16px] outline-none ml-2 w-full' 
+                onChange={(event)=>{
+                    setSearchQuery(event.target.value); //Every time user changes input in search bar, we want to save this as a possible search query
+                }}
             />
-        </div>
+        </form>
 
         {/*Tags to filter btwn diff blog sections. Map each item in tags array as a new bullet in
         unordered list. We want to display the name of the section, so item.name. 
@@ -81,29 +61,12 @@ function Search({selectedTag, onSearch}) {
         <div className = 'flex justify-center w-full gap-2 mt-3 sm:gap-4 sm:mt-4 md:gap-6 md:mt-4 lg:gap-8 lg:mt-5'>
             
                 {/*Add index as another parameter, since we were able to save this activeIndex */}
-                {/*map creates a new array of elements, new array of tags, where we save
-                the index for each of these items along with the item object in tags */}
                 {tags.map((item, index)=>(
-                    /*Basically means, when a user clicks on a button, will save that button's index 
-                    as the current activeIndex. So if the activeIndex that user clicked is same as index of
-                    button, this this button lights up(light blue not white). Basically, whenever a user selects a button,
-                    button clicked will light up. React can distinguish btwn each button's index
-                    because we created an event handler for each button.) */
-                    /*Using null in JSX means render nothing. So when button isn't clicked, we know since our activeIndex isn't that button's index,
-                    then no additional styling applied. */
-                    /*Remember, general padding increases size of buttons since we are adding space inside element */
-                    /*Generally buttons have small roundness, but button is rounded when screen size
-                    is medium+. Only added left right padding when screen size medium. IF too much left right padding
-                    when screen size small, buttons will overflow and become rlly long on page */
-                    /*When hover over button, button becomes 10% bigger, add thin border */
-                    /*Add smooth transation when hover, so styles don't apply too fast/harshly */
-                    /*We can use key to define all our buttons(tags) uniquely, and lets use item.id(refers to each specific tag.id in tags) */
-
                     <ul key={item.id}
                         onClick={() => { 
                             setActiveIndex(index); 
                             console.log("selected tag:", item.name);
-                            selectedTag(item.name) 
+                            onSelectedTag(item.name) 
                         }}
                         className={`${index == activeIndex ?
                             'bg-lightblue text-white' : null} 
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -15,11 +15,13 @@ function Home() {
     ? import.meta.env.VITE_BACKEND_PROD_URL : import.meta.env.VITE_BACKEND_URL;
     console.log(backendLink);
   
-  //Create a state, declare a list state variable.
-  //Start with an empty posts(array) because we will fetch our data from backend to fill our posts array
-  //We will use our second function to alter our state variable
-  const[posts, setPosts] = useState([]);
-  const[orgPosts, setOrgPosts] = useState([]);
+  const[posts, setPosts] = useState([]); //What is actually rendered
+  const[originalPosts, setOriginalPosts] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+
+  //Define a search query state, for getting input of searchbar in Home component
+  const [query, setQuery] = useState("");
+  const [selectedTag, setSelectedTag] = useState("All");
 
   //Use useEffect() hook so everytime webpage reloads, we call our backendAPI of getting all posts
   //Need to pass an empty array, so hook only executes once when page reloads.
@@ -28,8 +30,6 @@ function Home() {
   //Our backend data of all posts is sent to frontend as response variable
   //response.data gets our actual data. Our actual data of all our post documents is returned as an array of post objects
   useEffect(()=>{
-    
-    
     //Wrap our get request into a fetchPosts function.
     //We want to make this function Async, so we can use the await keyword on our get request.
     //We want to make sure our program doesn't try to generate html b4 we have sucessfully saved
@@ -40,7 +40,6 @@ function Home() {
       //Add error handling in case if our program doesn't fetch data sucessfully from backend
       //We will save our get request into a variable(result), so we can catch an error if get request fails
       try{
-
         const result = await Axios.get(`${backendLink}/getPosts`).then((response)=>{
           //Map each post in our posts array into a more enhanced post object
           //The map function returns a new array of our enhanced post objects
@@ -56,11 +55,8 @@ function Home() {
 
           //We will update our state variable posts(array) with this new array of enhanced post objects
           setPosts(enhancedPosts); //setPosts for keeping track of our current posts(all posts, filtered posts, query posts,)
-          setOrgPosts(enhancedPosts); //setOrg Posts so we can always keep track of full list of original posts from API
-
+          setOriginalPosts(enhancedPosts); //setOrg Posts so we can always keep track of full list of original posts from API
         });
-
-
         
       }catch(error){
         console.log("Welp, posts data failed to fetch from backend.");
@@ -73,87 +69,36 @@ function Home() {
       
   }, []);
 
+  //Whenever searchResults or selectedTag changes, we want to update what is rendered on screen based on our current selected tag
+  //Dependencies for this effect also includes if our originalPosts or query changes
+  useEffect(()=>{
+    //1. Set a base array of posts
+    //This depends on if there is a search query(use searchResults) or if there is no valid searchQuery(use originalPosts)
+    let base;
+    if(query.trim() === ""){
+      base = originalPosts;
+    } else{
+      base = searchResults;
+    }
 
-  const filterPost = (tag) => {
+    let filteredPosts;
+    if(selectedTag === "All"){
+      filteredPosts = base;
+    }else{
+      filteredPosts = base.filter(p => p.tag === selectedTag);
+    }
 
-   
-      let filteredPosts;
-    
-      if (searchQuery) {
-        // Filter from returnedPosts if a search has been made
-        filteredPosts = tag === "All" ? returnedPosts : returnedPosts.filter(post => post.tag === tag);
-      } else {
-        // Otherwise, filter from orgPosts
-        filteredPosts = tag === "All" ? orgPosts : orgPosts.filter(post => post.tag === tag);
-      }
-    
-      console.log("Filtering posts for tag:", tag);
-      console.log("Filtered posts:", filteredPosts);
-    
-      // Always set a new array reference
-      //The spread operator ([...]) creates a new array reference, even if the content of the array is identical. 
-      //Ensures React detects the state change and rerenders the component.
-      setPosts([...filteredPosts]);
+    setPosts([...filteredPosts]);
+  }, [searchResults, selectedTag, originalPosts, query]);
 
   
-    
-
-    // if(tag == 'All'){
-    //   (searchQuery!=null)? setPosts(returnedPosts) : setPosts(orgPosts);
-    // }else{
-    //   const filteredPosts = ((searchQuery!=null)? returnedPosts : orgPosts).filter(post => tag == post.tag);
-    //   setPosts(filteredPosts);
-
-      
-    // }
-
-    
-
-
-    // //IF search made, filter posts based on our returnedPosts. post state is the returned posts
-    // if (searchQuery != null && returnedPosts != null) {
-      
-    //   if(tag == 'All'){
-    //     console.log("we are now in all search");
-    //     setPosts(returnedPosts);
-    //   }else{
-    //     console.log("we are now in returned tag:", tag);
-    //     console.log("we are now in returned tag with returned posts:", returnedPosts);
-    //     const filteredPosts = returnedPosts.filter(post => post.tag == tag);
-    //     console.log("We are now in returned tag with filtered posts:", filteredPosts);
-    //     setPosts(filteredPosts);
-    //   }
-
-    //   //IF no search made, filter our posts based on original posts. post state is original posts
-    // } else {
-    //   if (tag == 'All') {
-    //     console.log("we are now in all original posts");
-    //     setPosts(orgPosts);
-    //     return;
-    //   }
-
-    //   //So we only store our posts into filteredPosts array if the current post.tag is equal to the selected tag 
-    //   console.log("we are now in filter original posts");
-    //   const filteredPosts = orgPosts.filter(post => post.tag == tag);
-    //   setPosts(filteredPosts);
-    // }
-
-  }
-
-  //Define a searchQuery state, for getting input of searchbar
-  //For search bar results, update our already created posts state
-  //Start searchbar input value be null
-  const [searchQuery, setSearchQuery] = useState(null);
-  const[returnedPosts, setReturnedPosts] = useState(null);
-  
-
   //Search Logic for searchbar
   const handleSearch = async (searchQuery)=>{
     
     //UPDATE searchQuery state in Home.jsx. 
     //At the moment we are just passing the searchQuery state up from Search.jsx, so in this function we are just using the argument passed up
     //As a result, after exiting this function, we still want the searchQuery state from Search.jsx in our Home.jsx, so update here too
-    setSearchQuery(searchQuery);
+    setQuery(searchQuery);
     
     //1. Get posts that match searchQuery or empty array if no matching posts found
     try{
@@ -169,15 +114,8 @@ function Home() {
           dateCreated: post.dateCreated,
           tag: post.section,
         }));
-        setReturnedPosts(enhancedResultPosts); //Update our resultPosts state
-        setPosts(enhancedResultPosts); //Update our posts state, our posts state is now just the returned posts. Remember we have a orgPosts and returnedPosts to keep our original posts and returned posts data intact
-        console.log(searchQuery); //posts state is for our current posts atm
-        console.log(enhancedResultPosts); //good
-        console.log("returned posts: ");
-        console.log(returnedPosts); //good
-        console.log("searchQuery now: "); //good
-        console.log(searchQuery);
-        filterPost("All");
+        setSearchResults(enhancedResultPosts); //Update our current posts state
+        setSelectedTag("All");
       });
 
     }catch(error){ //Failed to make API get request
@@ -190,11 +128,11 @@ function Home() {
   const renderPosts = ()=>{
 
     console.log("We are now in render posts!!");
-    console.log("searchQuery", searchQuery);
-    console.log("returnedPosts", returnedPosts);
+    console.log("search query", query);
+    console.log("posts", posts);
 
     //1. No search made, display normal posts
-    if(searchQuery == null){
+    if(query.trim() === ""){
       console.log("Displaying normal posts");
       return (
         <>
@@ -209,28 +147,28 @@ function Home() {
       );
 
     //2. search made & found posts matching query 
-    }else if(searchQuery != null && returnedPosts != null && returnedPosts.length >0){
+    }else if(query.trim() !== "" && posts != null && posts.length >0){
       console.log("Displaying search posts");
       return(
         <>
           <div className="flex justify-center mt-[15px]">
             <p className="text-[20px] mb-[15px]">{/*Use strong to inline bold */}
-              <strong>{returnedPosts.length}</strong> posts found for "{searchQuery}"
+              <strong>{posts.length}</strong> posts found for "{query}"
             </p>
           </div>
          
 
-          {returnedPosts.length>0? <IntroPost post={returnedPosts[0]}/> : null}
-          {returnedPosts.length>1? <Blogs posts = {returnedPosts.slice(1)}/>: null}
+          {posts.length>0? <IntroPost post={posts[0]}/> : null}
+          {posts.length>1? <Blogs posts = {posts.slice(1)}/>: null}
         </>
       );
 
-    }else if(searchQuery != null && returnedPosts != null && returnedPosts.length == 0){
+    }else if(query.trim() !== "" && posts != null && posts.length == 0){
       console.log("Displaying no results found");
       return(
         <>
           <p className= "text-[20px] text-center mt-[20px]">
-            No matches found for "{searchQuery}"
+            No matches found for "{query}"
           </p>
         </>
       );
@@ -245,7 +183,7 @@ function Home() {
         {/*Multiple props syntax: nameOfProp = {}, nameOfProp2={} */}
         {/*handleSearch function prop, bc altho searchbar in child, we still want to be able to access our resultPosts state in Home.jsx parent*/}
         {/*So, when onSearch invoked in child(since this is prop name), will call handleClick function here in parent & handle logic */}
-        <Search selectedTag={(tag)=>(filterPost(tag))} onSearch={(searchQuery)=>{handleSearch(searchQuery)}}/>
+        <Search onSelectedTag={(t)=>{setSelectedTag(t)}} onSearch={(searchQuery)=>{handleSearch(searchQuery)}}/>
 
         {renderPosts()}
 
